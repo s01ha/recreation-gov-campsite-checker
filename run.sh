314299c40e0a2263a33531647f9b52c6d69ef791
Sol HA <amoros@gmail.com>
2025-03-29 00:44:19 -0700

    Add Selenium host and authentication parameters to run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/bin/python3 /app/run.py --start-date ${START_DATE} --end-date ${END_DATE} --parks ${PARKS} --nights=${NIGHTS} --show-campsite-info --bot-token=${BOT_TOKEN} --chat-id=${CHAT_ID}
+/usr/local/bin/python3 /app/run.py --start-date ${START_DATE} --end-date ${END_DATE} --parks ${PARKS} --nights=${NIGHTS} --show-campsite-info --bot-token=${BOT_TOKEN} --chat-id=${CHAT_ID} --selenium-host ${SELENIUM_HOST} --username ${USERNAME} --password ${PASSWORD}
