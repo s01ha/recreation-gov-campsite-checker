0afe42ba8ff629c47e9d26578f63d92005efee06
Sol HA <amoros@gmail.com>
2025-03-30 15:03:34 -0700

    Add option to exclude specific campsite types in run script

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-/usr/local/bin/python3 /app/run.py --start-date ${START_DATE} --end-date ${END_DATE} --parks ${PARKS} --nights=${NIGHTS} --show-campsite-info --bot-token=${BOT_TOKEN} --chat-id=${CHAT_ID} --selenium-host ${SELENIUM_HOST} --username ${USERNAME} --password ${PASSWORD}
+/usr/local/bin/python3 /app/run.py --start-date ${START_DATE} --end-date ${END_DATE} --parks ${PARKS} --nights=${NIGHTS} --show-campsite-info --campsite-type-excluded ${CAMPSITE_TYPE_EXCLUDE} --bot-token=${BOT_TOKEN} --chat-id=${CHAT_ID} --selenium-host ${SELENIUM_HOST} --username ${USERNAME} --password ${PASSWORD}
